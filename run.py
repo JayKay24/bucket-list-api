677faf45880da329ee101ae8b9d57d387661d9e0
Jimmy <jameskinyua590@gmail.com>
2017-08-18 14:17:16 +0300

    [CHORE] Pass the created flask app instance to the JWTManager class

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from flask_jwt import JWT
 from app import create_app
+from flask_jwt_extended import JWTManager
 import views
 
 app = create_app('config')
-jwt = JWT(app, views.authenticate, views.identity)
+# jwt = JWT(app, views.authenticate, views.identity)
+jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run(host=app.config['HOST'],
