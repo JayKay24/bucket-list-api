6b59f2457220010bace6a319ae7bc1d92117ad45
Jimmy <jameskinyua590@gmail.com>
2017-08-20 11:49:17 +0300

    [CHORE] Remove the jwt_required decorator from the get method of the userlist resource

--- views.py
+++ views.py
@@ -35,7 +35,6 @@ class UserResource(Resource):
         return result
 
 class UserListResource(Resource):
-    @jwt_required
     def get(self):
         """
         Retrieves a paginated result set of users.
