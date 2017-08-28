d6cbdb873f9c2671e300e9dfe21d75206e6f922b
Jimmy <jameskinyua590@gmail.com>
2017-08-28 17:17:35 +0300

    [CHORE] Define a helper method get_a_list_of_bucketlists

--- tests/test_views.py
+++ tests/test_views.py
@@ -112,6 +112,13 @@ class ViewsTests(unittest.TestCase):
             headers=self.authorization)
         return response
 
+    def get_a_list_of_bucketlists(self):
+        url = url_for('api.bucketlistlistresource', _external=True)
+        response = self.test_client.get(
+            url,
+            headers=self.authorization)
+        return response
+
     def login_user(self, username, password):
         url = '/api/v1/auth/login/'
         data = {'username': username, 'password': password}
